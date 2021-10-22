04facfb01760d6bbd9f53c67ba7218d22449df51
David Abbey <davidabbey@earthlink.net>
2021-10-22 09:37:20 -0700

    get something turned in

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -2,27 +2,34 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 const dbData = require('../db/db.json')
-// (../db/db.json')
-
-
-
-router.get('/', (req, res) => res.json(dbData))
-
-router.get("/:id", (req, res) => {
-    for (let i = 0; i < dbData.length; i++) {
-        if (dbData[i].id == req.params.id) {
-            return res.json(dbData[i])
-        }
-    }
-    res.status(404).send("No note found")
+const { v4: uuidv4 } = require('uuid')
+ 
+router.get('/', (req, res) => {
+    let info = JSON.parse(fs.readFileSync(dbData, 'utf8'))
+    res.json(info)
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    dbData.push(req.body)
+    let newNote = req.body
+    console.log(newNote)
+    newNote.id = uuidv4()
+    let noteData = JSON.parse(fs.readFileSync(dbData, 'utf8'))
+    dbData.push(noteData)
+
     fs.writeFileSync('../db/db.json', JSON.stringify(dbData, null, 4))
     console.log("Done")
-    res.status(200).json(req.body)
+    res.status(200).json(noteData)
+})
+
+
+router.delete("/:id", (req, res) => {
+    let noteID = req.params.id.toString()
+    let noteData = JSON.parse(fs.readFileSync(dbData, 'utf8'))
+
+    const newData = noteData.filter(note => note.id.toString() !== noteID)
+
+    fs.writeFileSync('../db/db.json', JSON.stringify(newData, null, 4))
+    res.json(newData)
 })
 
 module.exports = router
